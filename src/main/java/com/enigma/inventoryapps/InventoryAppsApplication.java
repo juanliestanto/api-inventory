b128d1d8064a1b2a3e5a534be95c3e70ba86085c
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-03-25 18:49:42 +0700

    initials commit

---
+++ src/main/java/com/enigma/inventoryapps/InventoryAppsApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.inventoryapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/inventoryapps/InventoryAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.inventoryapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
