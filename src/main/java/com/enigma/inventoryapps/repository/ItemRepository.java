82f2b588fad55bdfd1808099bbb600aec6c46574
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-03-27 08:44:59 +0700

    [IMPROVE] feature/item (fixing repository)

--- src/main/java/com/enigma/inventoryapps/repository/ItemRepository.java
+++ src/main/java/com/enigma/inventoryapps/repository/ItemRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, String> {
+     /*In ItemRepository, I Used All Methods in JpaRepository for actions to database,
+    if you are going to use one of these methods, you only call the method as necessary. For example to insert or update
+    data, you can use itemRepository.save(yourEntity)
+     */
 }
