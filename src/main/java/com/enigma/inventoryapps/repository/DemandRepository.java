14968878b12a96e6def8910f6e63642284525a4a
JuanLiestanto <@liestantojuandwiky@gmail.com>
2024-03-27 08:44:16 +0700

    [IMPROVE] feature/demand (fixing repository)

--- src/main/java/com/enigma/inventoryapps/repository/DemandRepository.java
+++ src/main/java/com/enigma/inventoryapps/repository/DemandRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DemandRepository extends JpaRepository<Demand, String> {
+     /*In DemandRepository, I Used All Methods in JpaRepository for actions to database,
+    if you are going to use one of these methods, you only call the method as necessary. For example to insert or update
+    data, you can use demandRepository.save(yourEntity)
+     */
 }
